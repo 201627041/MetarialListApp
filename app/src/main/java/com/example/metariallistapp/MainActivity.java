a238d90f8e806ad718cefac367bb6991f95b889e
201627041 <whtjdwo97@gmail.com>
2020-05-06 12:52:27 +0900

    initial commit

--- app/src/main/java/com/example/metariallistapp/MainActivity.java
+++ app/src/main/java/com/example/metariallistapp/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.metariallistapp;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    EditText editText2;
+    TextView textView;
+    SQLiteDatabase database;
+    String tableName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editTex2);
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.buttom);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                String databaseName = editText.getText().toString();
+                createDatabase(databaseName);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                tableName = editText2.getText().toString();
+                createTable(tableName);
+
+                insertRecord();
+            }
+        });
+    }
+    private void createDatabase(String name){
+        println("createDatabase 호출됨");
+
+        database = openOrCreateDatabase(name, MODE_PRIVATE,null);
+
+        println("데이터베이스 생성함" + name);
+    }
+    private void createTable(String name){
+        println("createTable 호출됨");
+
+        if(database == null)
+        {
+            println("데이터베이스를 먼저 생성하세요" + name);
+            return;
+        }
+
+        database.execSQL("create table if not exists" + name + "("+"_id interger PRIMATY KEY autoincrement, "+"name text,"+"age interger"+"mobile text)");
+        println("테이블 생성함");
+    }
+    private void insertRecord(){
+        println("호출됨.");
+        if(database == null)
+        {
+            println("데이터베이스를 먼저 생성하세요.");
+            return;
+        }
+
+        if(tableName == null)
+        {
+            println("테이블을 먼저 생성하세요");
+            return;
+        }
+        database.execSQL(("insert int" + tableName) + "(name,age,mobile)" + "values" +"('john', 20, '010-1000-1000')");
+        println("레코드 추가함.");
+    }
+    public void println(String data){
+        textView.append(data + "\n");
     }
 }
